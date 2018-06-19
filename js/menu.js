35aa77ade702e1085cdf5236f6e55351894f8187
Rebecca Dantas <rbxdan@gmail.com>
2018-06-19 12:38:41 -0300

    Função pulsate() para evitar repetição de código

--- js/menu.js
+++ js/menu.js
@@ -68,6 +68,8 @@ $(document).ready(function () {
 
 //Button texts
 
+//SKILLS
+
 $myCanvas.drawText({
     layer: true,
     name: 'skillsButton',
@@ -86,93 +88,51 @@ $myCanvas.drawText({
         window.location = 'skills.html'
     },
     mouseover: function (layer) {
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle1',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-        })
-            .animateLayer('circle1', {
-                radius: 120
-            }, 700, function (layer) {
-                $myCanvas.removeLayer('circle1')
-            })
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle2',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-            delay: function (layer) {
-                $(this).animateLayer('circle2', {
-                    radius: 120
-                }, 900, function (layer) {
-                    $myCanvas.removeLayer('circle2')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle2', 400)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle3',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle3', {
-                    radius: 120
-                }, 700, function (layer) {
-                    $myCanvas.removeLayer('circle3')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle3', 200)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle4',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle4', {
-                    radius: 120
-                }, 1000, function (layer) {
-                    $myCanvas.removeLayer('circle4')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle4', 100)
+        pulsate()
 
         $myCanvas.animateLayer('skills', {
             strokeWidth: 15,
             start: 0,
             end: 360
         }, 500)
+        .setLayer('projectsButton', {
+            visible: false
+        })
+        .setLayer('contactButton', {
+            visible: false
+        })
+        .setLayer('aboutButton', {
+            visible: false
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1.1,
+            x: $myCanvas.width() / 2 + 140
+        })
     },
     mouseout: function(layer) {
+        $myCanvas.animateLayer('centerCircle', {
+            scale: 1
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1,
+            x: $myCanvas.width() / 2 + 120
+        })
+
         $(this).animateLayer('skills', {
             strokeWidth: 8,
             start: 15,
             end: 80
         }, 500)
+        .setLayers({
+            visible: true
+        })
     },
     text: 'my skills'
 })
+
+//CONTACT
 
 $myCanvas.drawText({
     layer: true,
@@ -192,95 +152,51 @@ $myCanvas.drawText({
         window.location = 'contact.html'
     },
     mouseover: function (layer) {
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle1',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-        })
-            .animateLayer('circle1', {
-                radius: 120,
-                start: 0,
-                end: 360
-            }, 700, function (layer) {
-                $myCanvas.removeLayer('circle1')
-            })
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle2',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-            delay: function (layer) {
-                $(this).animateLayer('circle2', {
-                    radius: 120
-                }, 900, function (layer) {
-                    $myCanvas.removeLayer('circle2')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle2', 400)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle3',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle3', {
-                    radius: 120
-                }, 700, function (layer) {
-                    $myCanvas.removeLayer('circle3')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle3', 200)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle4',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle4', {
-                    radius: 120
-                }, 1000, function (layer) {
-                    $myCanvas.removeLayer('circle4')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle4', 100)
+        pulsate()
 
         $myCanvas.animateLayer('contact', {
             strokeWidth: 15,
             start: 0,
             end: 360
         }, 500)
+        .setLayer('skillsButton', {
+            visible: false
+        })
+        .setLayer('projectsButton', {
+            visible: false
+        })
+        .setLayer('aboutButton', {
+            visible: false
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1.1,
+            x: $myCanvas.width() / 2 + 140
+        })
     },
     mouseout: function(layer) {
+        $myCanvas.animateLayer('centerCircle', {
+            scale: 1
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1,
+            x: $myCanvas.width() / 2 + 120
+        })
+
         $(this).animateLayer('contact', {
             strokeWidth: 8,
             start: 100,
             end: 165
         }, 500)
+        .setLayers({
+            visible: true
+        })
     },
     text: 'contact me'
 })
+
+//ABOUT
 
 $myCanvas.drawText({
     layer: true,
@@ -300,93 +216,51 @@ $myCanvas.drawText({
         window.location = 'aboutme.html'
     },
     mouseover: function (layer) {
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle1',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-        })
-            .animateLayer('circle1', {
-                radius: 120
-            }, 700, function (layer) {
-                $myCanvas.removeLayer('circle1')
-            })
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle2',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-            delay: function (layer) {
-                $(this).animateLayer('circle2', {
-                    radius: 120
-                }, 900, function (layer) {
-                    $myCanvas.removeLayer('circle2')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle2', 400)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle3',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle3', {
-                    radius: 120
-                }, 700, function (layer) {
-                    $myCanvas.removeLayer('circle3')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle3', 200)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle4',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle4', {
-                    radius: 120
-                }, 1000, function (layer) {
-                    $myCanvas.removeLayer('circle4')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle4', 100)
+        pulsate()
 
         $myCanvas.animateLayer('about', {
             strokeWidth: 15,
             start: 0,
             end: 360
         }, 500)
+        .setLayer('skillsButton', {
+            visible: false
+        })
+        .setLayer('contactButton', {
+            visible: false
+        })
+        .setLayer('projectsButton', {
+            visible: false
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1.1,
+            x: $myCanvas.width() / 2 - 230
+        })
     },
     mouseout: function(layer) {
+        $myCanvas.animateLayer('centerCircle', {
+            scale: 1
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1,
+            x: $myCanvas.width() / 2 - 210
+        })
+
         $(this).animateLayer('about', {
             strokeWidth: 8,
             start: 195,
             end: 260
         }, 500)
+        .setLayers({
+            visible: true
+        })
     },
     text: 'about me'
 })
+
+//PROJECTS
 
 $myCanvas.drawText({
     layer: true,
@@ -406,92 +280,127 @@ $myCanvas.drawText({
         window.location = 'projects.html'
     },
     mouseover: function (layer) {
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle1',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-        })
-            .animateLayer('circle1', {
-                radius: 120
-            }, 700, function (layer) {
-                $myCanvas.removeLayer('circle1')
-            })
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle2',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 2,
-            delay: function (layer) {
-                $(this).animateLayer('circle2', {
-                    radius: 120
-                }, 900, function (layer) {
-                    $myCanvas.removeLayer('circle2')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle2', 400)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle3',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle3', {
-                    radius: 120
-                }, 700, function (layer) {
-                    $myCanvas.removeLayer('circle3')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle3', 200)
-
-        $myCanvas.drawArc({
-            layer: true,
-            name: 'circle4',
-            x: $myCanvas.width() / 2,
-            y: $myCanvas.height() / 2,
-            radius: 0,
-            strokeStyle: 'black',
-            strokeWidth: 3,
-            delay: function (layer) {
-                $(this).animateLayer('circle4', {
-                    radius: 120
-                }, 1000, function (layer) {
-                    $myCanvas.removeLayer('circle4')
-                })
-            }
-        })
-
-        $myCanvas.delayLayer('circle4', 100)
+        
+        pulsate()
 
         $myCanvas.animateLayer('projects', {
             strokeWidth: 15,
             start: 0,
             end: 360
         }, 500)
+        .setLayer('skillsButton', {
+            visible: false
+        })
+        .setLayer('contactButton', {
+            visible: false
+        })
+        .setLayer('aboutButton', {
+            visible: false
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1.1,
+            x: $myCanvas.width() / 2 - 250
+        })
     },
     mouseout: function(layer) {
+        $myCanvas.animateLayer('centerCircle', {
+            scale: 1
+        })
+
+        $(this).animateLayer(layer, {
+            scale: 1,
+            x: $myCanvas.width() / 2 - 230
+        })
+
         $(this).animateLayer('projects', {
             strokeWidth: 8,
             start: 280,
             end: 345
         }, 500)
+        .setLayers({
+            visible: true
+        })
     },
     text: 'my projects'
 })
+
+function pulsate() {
+    $myCanvas.drawArc({
+        layer: true,
+        name: 'circle1',
+        x: $myCanvas.width() / 2,
+        y: $myCanvas.height() / 2,
+        radius: 0,
+        strokeStyle: 'black',
+        strokeWidth: 2,
+    })
+        .animateLayer('circle1', {
+            radius: 120
+        }, 700, function (layer) {
+            $myCanvas.removeLayer('circle1')
+        })
+
+    $myCanvas.drawArc({
+        layer: true,
+        name: 'circle2',
+        x: $myCanvas.width() / 2,
+        y: $myCanvas.height() / 2,
+        radius: 0,
+        strokeStyle: 'black',
+        strokeWidth: 2,
+        delay: function (layer) {
+            $(this).animateLayer('circle2', {
+                radius: 120
+            }, 900, function (layer) {
+                $myCanvas.removeLayer('circle2')
+            })
+        }
+    })
+
+    $myCanvas.delayLayer('circle2', 400)
+
+    $myCanvas.drawArc({
+        layer: true,
+        name: 'circle3',
+        x: $myCanvas.width() / 2,
+        y: $myCanvas.height() / 2,
+        radius: 0,
+        strokeStyle: 'black',
+        strokeWidth: 3,
+        delay: function (layer) {
+            $(this).animateLayer('circle3', {
+                radius: 120
+            }, 700, function (layer) {
+                $myCanvas.removeLayer('circle3')
+            })
+        }
+    })
+
+    $myCanvas.delayLayer('circle3', 200)
+
+    $myCanvas.drawArc({
+        layer: true,
+        name: 'circle4',
+        x: $myCanvas.width() / 2,
+        y: $myCanvas.height() / 2,
+        radius: 0,
+        strokeStyle: 'black',
+        strokeWidth: 3,
+        delay: function (layer) {
+            $(this).animateLayer('circle4', {
+                radius: 120
+            }, 1000, function (layer) {
+                $myCanvas.removeLayer('circle4')
+            })
+        }
+    })
+
+    $myCanvas.delayLayer('circle4', 100)
+
+    $myCanvas.animateLayer('centerCircle', {
+        scale: 1.2
+    })
+}
 
 })
